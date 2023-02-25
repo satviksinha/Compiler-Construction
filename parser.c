a5e1d039e2119e19e67089ea0e77cce92584d627
satviksinha <f20200993@pilani.bits-pilani.ac.in>
2023-02-26 01:49:22 +0530

    firstSet completed

--- parser.c
+++ parser.c
@@ -1,40 +1,42 @@
-# include "parser.h"
-
+#include "parser.h"
 
 // hash function for non terminals
-int get_hash_nonTerminal(const char* s) 
+int get_hash_nonTerminal(const char *s)
 {
     int n = strlen(s);
     long long p = 31, m = 1157;
     long long hash = 0;
     long long p_pow = 1;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         hash = (hash + (s[i] - 'a' + 1) * p_pow) % m;
         p_pow = (p_pow * p) % m;
     }
     return abs(hash);
 }
 
-int get_hash_terminal(const char* s) 
+int get_hash_terminal(const char *s)
 {
     int n = strlen(s);
     long long p = 31, m = 1457;
     long long hash = 0;
     long long p_pow = 1;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         hash = (hash + (s[i] - 'a' + 1) * p_pow) % m;
         p_pow = (p_pow * p) % m;
     }
     return abs(hash);
 }
 
-int get_hash(const char* s) 
+int get_hash(const char *s)
 {
     int n = strlen(s);
     long long p = 31, m = 10e4;
     long long hash = 0;
     long long p_pow = 1;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         hash = (hash + (s[i] - 'a' + 1) * p_pow) % m;
         p_pow = (p_pow * p) % m;
     }
@@ -42,40 +44,40 @@ int get_hash(const char* s)
 }
 
 // function for storing grammar rules in the form of linked list
-void makeGrammar(FILE* fp)
+void makeGrammar(FILE *fp)
 {
     char buff[1024];
     int counter = 0;
-    char delim[] = {", \n"};
-    
-    while(fgets(buff, 1024, fp))
-    {
-        char * token = strtok(buff, delim);
-        struct node* nonTerminal = (struct node*) malloc(sizeof(struct node));
-        nonTerminal->isTerminal = 0;
-        //nonTerminal->value = token;
-        strcpy(nonTerminal->value,token);
-        grammar[counter] = nonTerminal;
-        struct node* curr_token = nonTerminal;
-        //printf("%s->",token);
+    char delim[] = {",\n"};
 
-        while((token = strtok(NULL, delim)) != NULL)
+    while (fgets(buff, 1024, fp))
+    {
+        char *token = strtok(buff, delim);
+        struct node *nonTerminal = (struct node *)malloc(sizeof(struct node));
+        nonTerminal->isTerminal = 0;
+        // nonTerminal->value = token;
+        strcpy(nonTerminal->value, token);
+        grammar[counter] = nonTerminal;
+        struct node *curr_token = nonTerminal;
+        // printf("%s->",token);
+
+        while ((token = strtok(NULL, delim)) != NULL)
         {
-            struct node* temp = (struct node*) malloc(sizeof(struct node));
-            if(islower(token[0])) 
-                temp->isTerminal = 0;  
+            struct node *temp = (struct node *)malloc(sizeof(struct node));
+            if (islower(token[0]))
+                temp->isTerminal = 0;
             else
                 temp->isTerminal = 1;
 
             // temp->value = token;
-            strcpy(temp->value,token);
+            strcpy(temp->value, token);
             curr_token->link = temp;
             curr_token = temp;
-            //printf("%s,",token);
+            // printf("%s,",token);
         }
         curr_token->link = NULL;
         counter++;
-        //printf("\n");
+        // printf("\n");
     }
 }
 
@@ -88,76 +90,89 @@ void makeGrammar(FILE* fp)
 //         {
 //             parseTable[get_hash_nonTerminal(grammar[i]->value)][get_hash_terminal(firstAndFollow[i][j])] = grammar[i];
 //         }
-//     }   
+//     }
 // }
-
 
 // int already_visited[128];
 char ntFirst[99500][300]; // size to be changed later
 int isEpsilon[99500];
-int createfirst(char* term){//returns 0 if first contains epsilon,otherwise returns 1
-    //printf("%s",term);
-    if(strlen(ntFirst[get_hash(term)])){
+int cnt = 0;
+int createfirst(char *term)
+{ // returns 0 if first contains epsilon,otherwise returns 1
+     //printf("%s",term);
+
+    if (strlen(ntFirst[get_hash(term)]))
+    {
         // printf("%s",term);
         return isEpsilon[get_hash(term)];
     }
-    printf("%d:%s",isupper(term[0]),term);
-    if(isupper(term[0])){       //if term is a terminal
-        if(strcmp(term,"EPSILON")){     //and its not an epsilon
-            if(!strlen(ntFirst[get_hash(term)]))  //if ntFirst of that term isnt empty  
-                strcat(ntFirst[get_hash(term)],term);   //append , and term
-            //printf("%s\n",ntFirst[get_hash(term)]);
+    // printf("%d:%s \n", isupper(term[0]), term);
+    if (isupper(term[0]))
+    { // if term is a terminal
+        if (strcmp(term, "EPSILON"))
+        {                                              // and its not an epsilon
+            if (!strlen(ntFirst[get_hash(term)]))      // if ntFirst of that term is empty
+                strcat(ntFirst[get_hash(term)], term); // append term
+             //printf("%s\n",ntFirst[get_hash(term)]);
             return 0;
         }
-        else{
-            isEpsilon[get_hash(term)] = 1; //if term is epsilon set isEpsilon 1 and return 1
+        else
+        {
+            isEpsilon[get_hash(term)] = 1; // if term is epsilon set isEpsilon 1 and return 1
             return 1;
         }
     }
 
-    //if term is a non terminal,find first 
-    for(int i=0;i<128;i++){  
-        if(!strcmp(grammar[i]->value,term)){  //matching nt found
-            struct node* temp = grammar[i]->link;     //LHS
-            while(temp!=NULL && createfirst(temp->value)){       //if temp is a terminal (except epsilon) or a nt (which doesnt derive epsilon)
-                if(strlen(ntFirst[get_hash(term)]) && ntFirst[get_hash(term)][strlen(ntFirst[get_hash(term)])-1]!=',')  //if ntFirst of that term isnt empty
-                    strcat(ntFirst[get_hash(term)],","); 
-                strcat(ntFirst[get_hash(term)],ntFirst[get_hash(temp->value)]);
+    // if term is a non terminal,find first
+    for (int i = 0; i < 129; i++)
+    {
+        if (!strcmp(grammar[i]->value, term))
+        {                                         // matching nt found
+            struct node *temp = grammar[i]->link; // LHS
+            while (temp != NULL && createfirst(temp->value))
+            {                                                                                                               // if temp is a terminal (except epsilon) or a nt (which doesnt derive epsilon)
+                if (strlen(ntFirst[get_hash(term)]) && ntFirst[get_hash(term)][strlen(ntFirst[get_hash(term)]) - 1] != ',') // if ntFirst of that term isnt empty
+                    strcat(ntFirst[get_hash(term)], ",");
+                strcat(ntFirst[get_hash(term)], ntFirst[get_hash(temp->value)]);
                 temp = temp->link;
             }
-            if(temp == NULL)
+            if (temp == NULL)
                 isEpsilon[get_hash(term)] = 1;
-            else{
-                if(strlen(ntFirst[get_hash(term)]) && ntFirst[get_hash(term)][strlen(ntFirst[get_hash(term)])-1]!=',')  //if ntFirst of that term isnt empty
-                    strcat(ntFirst[get_hash(term)],","); 
-                //printf("%s\n",ntFirst[get_hash(temp->value)]);
-                strcat(ntFirst[get_hash(term)],ntFirst[get_hash(temp->value)]);
+            else
+            {
+                if (strlen(ntFirst[get_hash(term)]) && ntFirst[get_hash(term)][strlen(ntFirst[get_hash(term)]) - 1] != ',') // if ntFirst of that term isnt empty
+                    strcat(ntFirst[get_hash(term)], ",");
+                // printf("%s\n",ntFirst[get_hash(temp->value)]);
+                strcat(ntFirst[get_hash(term)], ntFirst[get_hash(temp->value)]);
             }
         }
     }
     return isEpsilon[get_hash(term)];
 }
 
-void computeFirstAndFollow(){
-    for(int i=0;i<67;i++){
-        char* non_terminal = nonTerminals[i];
-        createfirst(non_terminal); //calculate first of lhs 
+void computeFirstAndFollow()
+{
+    for (int i = 0; i < 67; i++)
+    {
+        char *non_terminal = nonTerminals[i];
+        createfirst(non_terminal); // calculate first of lhs
     }
 }
 
 int main()
 {
-    FILE* fp = fopen("grammar.txt","r");
+    FILE *fp = fopen("grammar.txt", "r");
     makeGrammar(fp);
     // memset(ntFirst,'-',1157*300);
-    memset(isEpsilon,0,sizeof(isEpsilon));
-    // for(int i=0;i<125;i++){
-    //     char* non_terminal = nonTerminals[i];
-    //     createfirst(non_terminal); //calculate first of lhs 
-    // }
-    createfirst("ioStmt");
-    // for(int i=0;i<125;i++)
-    //     printf("%s:%s\n",nonTerminals[i],ntFirst[get_hash(nonTerminals[i])]);
-     //printf("%s",ntFirst[get_hash("ioStmt")]);
+    memset(isEpsilon, 0, sizeof(isEpsilon));
+    for (int i = 0; i < 125; i++)
+    {
+        char *non_terminal = nonTerminals[i];
+        createfirst(non_terminal); // calculate first of lhs
+    }
+    // createfirst("arithmeticOrBooleanExpr");
+    for (int i = 0; i < 125; i++)
+        printf("%s:%s\n", nonTerminals[i], ntFirst[get_hash(nonTerminals[i])]);
+    // printf("%s", ntFirst[get_hash("arithmeticOrBooleanExpr")]);
     return 0;
 }
--- parser.h
+++ parser.h
@@ -23,11 +23,11 @@ struct node
 // "declareStmt","conditionalStmt","caseStmt","post","value","default","iterativeStmt",
 // "range_for","range"};
 
-char nonTerminals[125][23] = {"startprogram","program","moduleDeclarations","moduleDeclaration","otherModules",
+char nonTerminals[125][24] = {"startprogram","program","moduleDeclarations","moduleDeclaration","otherModules",
 "driverModule","module","ret","input_plist","moreList","output_plist","moreOutput",
 "dataType","type","moduleDef","statements","statement","ioStmt","print_var","whichId2",
 "boolvar","whichId","index","sign","aVar","var_id_num","newArithmeticExpr","startExpr",
-"newA1","newA2","newTerm","newNextTerm","u1","after_u1","simpleStmt","asssignmentStmt",
+"newA1","newA2","newTerm","newNextTerm","u1","after_u1","simpleStmt","assignmentStmt",
 "whichStmt","lvalueIDStmt","lvalueARRStmt","moduleReuseStmt","optional","idList",
 "moreId","expression","arithmeticOrBooleanExpr","ab1","anyTerm","ab2","u","after_unary",
 "arithmeticExpr","a1","term","a2","nextTerm","op1","op2","bop","relationalOp",
@@ -45,9 +45,9 @@ char terminals[57][12] = {"INTEGER","REAL","BOOLEAN","OF","ARRAY","START","END",
 "COMMA","ASSIGNOP","SQBO","SQBC","BO","BC","COMMENTMARK"};
 
 
-struct node* grammar[128];
+struct node* grammar[129];
 // Doubt - how to declare array?
-char firstAndFollow[128][111];
+char firstAndFollow[129][111];
 struct node* parseTable[100][100];
 
 // function for storing grammar rules in the form of linked list
